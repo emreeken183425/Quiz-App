377e23f52d0a02b8e2b61b7ce53b871ddb754eba
emreeken183425 <emreeken1834@gmail.com>
2022-09-26 20:19:27 +0300

    models

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,24 @@
+from tabnanny import verbose
+from unicodedata import category
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name=models.CharField(max_length=50 ,verbose_name='Category Name' )#verbose_name='Category Name' admin panelde nasıl görünmesini istiyorsak bunu yazarız 
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural='Categories'
+        
+        
+        
+class Quiz(models.Model):
+    title=models.CharField(max_length=50 ,verbose_name='Quiz  title'  )  
+    category=models.ForeignKey(Category,on_delete=models.CASCADE   )      
+        
+        
+        
+        
+        
