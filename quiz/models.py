8ad92833f6a72f88328f829a163aa83e80e602a0
emreeken183425 <emreeken1834@gmail.com>
2022-09-26 20:26:41 +0300

    questions

--- quiz/models.py
+++ quiz/models.py
@@ -1,4 +1,5 @@
 from tabnanny import verbose
+from turtle import update
 from unicodedata import category
 from django.db import models
 
@@ -16,8 +17,27 @@ class Category(models.Model):
         
 class Quiz(models.Model):
     title=models.CharField(max_length=50 ,verbose_name='Quiz  title'  )  
-    category=models.ForeignKey(Category,on_delete=models.CASCADE   )      
+    category=models.ForeignKey(Category,on_delete=models.CASCADE   ) 
+    created=models.DateTimeField(auto_now_add=True )
+    updated=models.DateTimeField(auto_now=True )
+    def __str__(self):
+        return self.title 
+    
+    class Meta:
+        verbose_name_plural='Quizzes'    
         
+        
+class Question(models.Model):
+    SCALE={
+        ('B','Beginner'),
+        ('I','Intermediate'),
+        ('A','Advanced')
+    }
+    title=models.TextField( )
+    quiz=models.ForeignKey(Quiz, on_delete=models.CASCADE )
+    difficulty= models.CharField(max_length=1 , choices=SCALE  )        
+    created=models.DateTimeField(auto_now_add=True )
+    updated=models.DateTimeField(auto_now=True )
         
         
         
